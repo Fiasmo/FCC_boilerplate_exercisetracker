2906f23455eaedef490258b1cdf816246c574ac7
Marcel Roth <Fiasmo.mr@gmail.com>
2024-07-03 19:43:54 +0200

    final again
    aaa

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
 let bodyParser = require('body-parser');
 
 let mongoose;
